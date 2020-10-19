d3619244d1d7a26d6d3f078fdf3734c6693bbcba
Sinhika <sinhika@republicofnewhome.org>
2020-10-19 12:44:13 -0500

    added a version number to built pack
    (cherry picked from commit f951760606a76ffb2bf9a12e59f0cdf7348f2c6f)

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@
 VERSION=1.14.4
 BRANCH=1.14
 ARCNAME=../SimpleOresClassic-${VERSION}.zip
+BUILD=2
+ARCNAME=../SimpleOresClassic-${VERSION}v${BUILD}.zip
 git archive --format=zip -o ${ARCNAME} ${BRANCH}
